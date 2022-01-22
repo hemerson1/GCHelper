ca5409e125735836004fd2c8c14221c8d8671ef4
hemerson1 <emersonharry8@gmail.com>
2022-01-22 11:17:34 +0000

    Added a testing function

--- GCHelper/Evaluation.py
+++ GCHelper/Evaluation.py
@@ -12,23 +12,43 @@ Functions for evaluating algorithmic performance and displaying it to the user.
 
 import matplotlib.pyplot as plt
 import numpy as np
+import torch, random
 
-# TODO: add the testing function
-# will need to incorporate a way of getting the agent action
+from General import PID_action, calculate_bolus, calculate_risk
 
-def test_algorithm(env, model, seed=0, max_timesteps=480, sequence_length=80,
-                   data_processing="condensed", pid_run=False, params):
+
+"""
+Test the learned policy of an agent against the PID algorithm over a 
+specified length of time.
+"""
+def test_algorithm(env, agent, seed=0, max_timesteps=480, sequence_length=80,
+                   data_processing="condensed", pid_run=False, params=None):
     
     # Unpack the params
-    default_basal = params.get("default_basal")  
+    
+    # Diabetes
+    basal_default = params.get("basal_default") 
+    target_blood_glucose = params.get("target_blood_glucose", 144)
+    
+    # Bolus
+    cr = params.get("carbohydrate_ratio")
+    cf = params.get("correction_factor")
+    
+    # PID
+    kp = params.get("kp")
+    ki = params.get("ki")
+    kd = params.get("kd")
+    
+    # Means and Stds
     state_mean = params.get("state_mean")  
     state_std = params.get("state_std")  
-    
+    action_mean = params.get("action_mean")  
+    action_std = params.get("action_std")
     
     # initialise the arrays for data collection    
     rl_reward, rl_blood_glucose, rl_action = 0, [], []
     pid_reward, pid_blood_glucose, pid_action = 0, [], []
-    rl_action, rl_meals = [], []
+    rl_insulin, rl_meals = [], []
 
     # select the number of iterations    
     if pid_run: runs = 2
@@ -45,7 +65,7 @@ def test_algorithm(env, model, seed=0, max_timesteps=480, sequence_length=80,
         # Initialise the environment --------------------------------------
         
         # get the state
-        insulin_dose = 1/3 * default_basal
+        insulin_dose = 1/3 * basal_default
         meal, done, bg_val = 0, False, env.reset()
         time = ((env.env.time.hour * 60) / 3 + env.env.time.minute / 3) / 479
         state = np.array([bg_val[0], meal, insulin_dose, time], dtype=np.float32)
@@ -65,8 +85,7 @@ def test_algorithm(env, model, seed=0, max_timesteps=480, sequence_length=80,
         # intiialise pid parameters
         integrated_state = 0
         previous_error = 0
-        timesteps = 0
-        
+        timesteps = 0        
         
         while not done and timesteps < max_timesteps:
             
@@ -93,15 +112,11 @@ def test_algorithm(env, model, seed=0, max_timesteps=480, sequence_length=80,
                 # Normalise the current state
                 state = (state - state_mean) / state_std
                 
-                if self.data_processing == "sequence":
-                    model.init_hidden(1)
-                
-                with torch.no_grad():
-                    state = torch.tensor(state, dtype=torch.float32, device=self.device)
-                    action = model(state)
+                # get the action prediction from the model
+                action = agent.select_action(state)
                                         
                 # Unnormalise action output  
-                action_pred = (action.cpu().data.numpy().flatten() * self.action_std + self.action_mean)[0]
+                action_pred = (action.cpu().data.numpy().flatten() * action_std + action_mean)[0]
                 
                 # to stop subtracting from bolus when -ve
                 action_pred = max(0, action_pred)
@@ -110,14 +125,32 @@ def test_algorithm(env, model, seed=0, max_timesteps=480, sequence_length=80,
 
             # Run the pid algorithm ------------------------------------------------------
             else:                            
-                player_action = self.PID_action(bg_val)
-
+                player_action, previous_error, integrated_state = PID_action(
+                    blood_glucose=bg_val, previous_error=previous_error, 
+                    integrated_state=integrated_state, 
+                    target_blood_glucose=target_blood_glucose, 
+                    kp=kp, ki=ki, kd=kd, basal_default=basal_default
+                    )
+                
+    
             # update the chosen action
             chosen_action = np.copy(player_action)
+            
+            # Get the meal bolus --------------------------------------------
 
             # take meal bolus
-            if meal > 0:                             
-                chosen_action = float(chosen_action) + self.calculate_bolus(bg_val, meal_history, meal)
+            if meal > 0:      
+
+                bolus_action = calculate_bolus(
+                    blood_glucose=bg_val, meal_history=meal_history,
+                    current_meal=meal,carbohyrdate_ratio=cr, 
+                    correction_factor=cf, 
+                    target_blood_glucose=target_blood_glucose
+                    ) 
+                                       
+                chosen_action = float(chosen_action) + bolus_action
+                
+            # Step the environment ------------------------------------------                
 
             # append the basal and bolus action
             action_stack = np.delete(action_stack, 0, 0)
@@ -125,11 +158,11 @@ def test_algorithm(env, model, seed=0, max_timesteps=480, sequence_length=80,
 
             # step the simulator
             next_bg_val, _, done, info = env.step(chosen_action)
-            reward = -self.calculate_risk(next_bg_val) 
+            reward = -calculate_risk(next_bg_val) 
 
             # get the rnn array format for state
-            input_time = ((env.env.time.hour * 60) / 3 + env.env.time.minute / 3) / 479
-            next_state = np.array([float(next_bg_val[0]), float(info['meal']), float(chosen_action), input_time], dtype=np.float32)   
+            time = ((env.env.time.hour * 60) / 3 + env.env.time.minute / 3) / 479
+            next_state = np.array([float(next_bg_val[0]), float(info['meal']), float(chosen_action), time], dtype=np.float32)   
 
             # update the state stacks
             next_state_stack = np.delete(state_stack, 0, 0)
@@ -141,178 +174,39 @@ def test_algorithm(env, model, seed=0, max_timesteps=480, sequence_length=80,
             if done: 
                 reward = -1e5
                 break
-        
-
-if not training:  
             
-            rnn = ""
-            if self.data_processing == "sequence":
-                rnn = "_RNN" 
+            # Save the testing results --------------------------------------
+
+            # for RL agent
+            if ep == 0:
+                rl_blood_glucose.append(next_bg_val[0])
+                rl_action.append(player_action)
+                rl_insulin.append(chosen_action)
+                rl_reward += reward
+                rl_meals.append(info['meal'])
+
+            # for pid agent
+            else:
+                pid_blood_glucose.append(next_bg_val[0])
+                pid_action.append(player_action)
+                pid_reward += reward
+                
+                
+            # Update the state ---------------------------------------------
+
+            # update the meal history
+            meal_history = np.append(meal_history, meal)
+            meal_history = np.delete(meal_history, 0)   
+
+            # update the state stacks
+            state_stack = next_state_stack
+
+            # update the state
+            bg_val, state, meal = next_bg_val, next_state, info['meal']
+            timesteps += 1 
             
-            # load the learned model
-            self.load_model('./Models/BC' + rnn + '_weights')            
-            
-        if self.data_processing == "condensed":
-            model = self.model
-            
-        elif self.data_processing == "sequence":
-            model = self.RNN_model            
-            
-        # TESTING --------------------------------------------------------------------------------------------
+    return rl_reward, rl_blood_glucose, rl_action, rl_insulin, rl_meals, pid_reward, pid_blood_glucose, pid_action
 
-        test_seed = 0
-        env = gym.make('simglucose-child1-v0')
-        max_timesteps = 480
-
-        runs = 2
-        if self.pid_run:
-            runs = 1
-
-        # get the rl arrays
-        rl_reward = 0
-        rl_bg = []
-        rl_insulin = []
-        rl_action = []
-        rl_meals = []
-
-        # stop the pid re-running
-        self.pid_run = True  
-        
-        for ep in range(runs):
-
-            # reset the seed
-            env.seed(test_seed)  
-            np.random.seed(test_seed)
-            torch.manual_seed(test_seed) 
-            random.seed(test_seed)
-            
-            # Preprocess the state ----------------------------------------------------------
-
-            # get the state
-            insulin_dose = 1/3 * self.bas
-            meal, done, bg_val = 0, False, env.reset()
-            input_time = ((env.env.time.hour * 60) / 3 + env.env.time.minute / 3) / 479
-            state = [bg_val[0], meal, insulin_dose, input_time]    
-            state = np.array(state, dtype=np.float32)
-
-            # get a suitable input
-            state_stack = np.tile(state, (self.sequence_length + 1, 1))
-
-            # ensure that the time is correct
-            state_stack[:, 3] = (state_stack[:, 3] - np.arange(((self.sequence_length + 1) / 479), 0, -(1 / 479))[:self.sequence_length + 1]) * 479            
-            state_stack[:, 3] = (np.around(state_stack[:, 3], 0) % 480) / 479         
-
-            # get the action and reward stack
-            action_stack = np.tile(np.array([insulin_dose], dtype=np.float32), (self.sequence_length + 1, 1))        
-            reward_stack = np.tile(-self.calculate_risk(bg_val), (self.sequence_length + 1, 1))
-
-            # get the meal history
-            meal_history = np.zeros(int((3 * 60) / 3), dtype=np.float32)
-
-            self.integrated_state = 0
-            self.previous_error = 0
-            timesteps = 0
-
-            while not done and timesteps < max_timesteps:
-
-                # Run the RL algorithm ------------------------------------------------------
-                if ep == 0:
-                    
-                    if self.data_processing == "condensed":
-                                        
-                        # Process the current state
-                        bg_vals = state_stack[:, 0][::10]                    
-                        meal_vals = state_stack[:, 1]
-                        insulin_vals = state_stack[:, 2]
-                        meals_on_board = np.sum(meal_vals * np.arange(1 / (self.sequence_length + 2), 1, 1/(self.sequence_length + 2)))
-                        insulin_on_board = np.sum(insulin_vals * np.arange(1 / (self.sequence_length + 2), 1, 1/(self.sequence_length + 2)))                     
-                        state = np.concatenate([bg_vals, meals_on_board.reshape(1), insulin_on_board.reshape(1)])
-                        
-                    elif self.data_processing == "sequence":
-                        state = state_stack[1:, :3].reshape(1, self.sequence_length, 3) 
-                                                
-                    # Normalise the current state
-                    state = (state - self.state_mean) / self.state_std
-                    
-                    if self.data_processing == "sequence":
-                        model.init_hidden(1)
-                    
-                    with torch.no_grad():
-                        state = torch.tensor(state, dtype=torch.float32, device=self.device)
-                        action = model(state)
-                                            
-                    # Unnormalise action output  
-                    action_pred = (action.cpu().data.numpy().flatten() * self.action_std + self.action_mean)[0]
-                    
-                    # to stop subtracting from bolus when -ve
-                    action_pred = max(0, action_pred)
-                    player_action = action_pred
-
-                # Run the pid algorithm ------------------------------------------------------
-                else:                            
-                    player_action = self.PID_action(bg_val)
-
-                # update the chosen action
-                chosen_action = np.copy(player_action)
-
-                # take meal bolus
-                if meal > 0:                             
-                    chosen_action = float(chosen_action) + self.calculate_bolus(bg_val, meal_history, meal)
-
-                # append the basal and bolus action
-                action_stack = np.delete(action_stack, 0, 0)
-                action_stack = np.vstack([action_stack, player_action])
-
-                # step the simulator
-                next_bg_val, _, done, info = env.step(chosen_action)
-                reward = -self.calculate_risk(next_bg_val) 
-
-                # get the rnn array format for state
-                input_time = ((env.env.time.hour * 60) / 3 + env.env.time.minute / 3) / 479
-                next_state = np.array([float(next_bg_val[0]), float(info['meal']), float(chosen_action), input_time], dtype=np.float32)   
-
-                # update the state stacks
-                next_state_stack = np.delete(state_stack, 0, 0)
-                next_state_stack = np.vstack([next_state_stack, next_state]) 
-                reward_stack = np.delete(reward_stack, 0, 0)
-                reward_stack = np.vstack([reward_stack, np.array([reward], dtype=np.float32)])
-
-                # add a termination penalty
-                if done: 
-                    reward = -1e5
-                    break
-
-                # ---------------------------------------
-
-                # for RL agent
-                if ep == 0:
-                    rl_bg.append(next_bg_val[0])
-                    rl_action.append(player_action)
-                    rl_insulin.append(chosen_action)
-                    rl_reward += reward
-                    rl_meals.append(info['meal'])
-
-                # for pid agent
-                else:
-                    self.pid_bg.append(next_bg_val[0])
-                    self.pid_insulin.append(chosen_action)
-                    self.pid_action.append(player_action)
-                    self.pid_reward += reward
-                    
-                # -----------------------------------------
-
-                # update the meal history
-                meal_history = np.append(meal_history, meal)
-                meal_history = np.delete(meal_history, 0)   
-
-                # update the state stacks
-                state_stack = next_state_stack
-
-                # update the state
-                bg_val = next_bg_val
-                state = next_state     
-                meal = info['meal']
-                timesteps += 1 
 
 """
 Plot a four-tiered graph comparing the blood glucose control of a PID 
@@ -321,7 +215,7 @@ carbohyrdates.
 """
 
 def create_graph(rl_reward, rl_blood_glucose, rl_action, rl_insulin, rl_meals,
-                 pid_reward, pid_blood_glucose, pid_action, default_basal):
+                 pid_reward, pid_blood_glucose, pid_action, basal_default):
     
     # TODO: add the function for printing metrics like TIR for PID vs RL
     
@@ -359,7 +253,7 @@ def create_graph(rl_reward, rl_blood_glucose, rl_action, rl_insulin, rl_meals,
         # show the basal doses
         axs[1].plot(x, pid_action, label='pid', color='orange')
         axs[1].plot(x, rl_action, label='rl', color='dodgerblue')
-        axs[1].axis(ymin=0.0, ymax=(default_basal * 1.4))
+        axs[1].axis(ymin=0.0, ymax=(basal_default * 1.4))
         axs[1].set_ylabel("Basal \n(U/min)")
 
         # show the bolus doses
